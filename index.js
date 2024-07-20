d5ba38f92097b1c03f17322cfa578300719394f1
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-20 13:09:58 +0300

    completes tests

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+    // Reverse the string
+    const reversedWord = word.split('').reverse().join('');
+    
+    // Compare original and reversed strings
+    return word === reversedWord;
 }
+
+console.log(isPalindrome("madam"));
+console.log(isPalindrome("robot"));
+console.log(isPalindrome("abba")); 
+console.log(isPalindrome("a")); 
+console.log(isPalindrome("ab")); 
 
 /* 
   Add your pseudocode here
@@ -11,15 +21,15 @@ function isPalindrome(word) {
 */
 
 // You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+// if (require.main === module) {
+//  add your own custom tests in here
+//   console.log("Expecting: true");
+//   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+//   console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
+//   console.log("Expecting: false");
+//   console.log("=>", isPalindrome("robot"));
+// }
 
-module.exports = isPalindrome;
+ module.exports = isPalindrome;
